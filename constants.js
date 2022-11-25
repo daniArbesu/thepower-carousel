14a772266b08780bba81622e40df3457eeeb2318
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2022-11-25 15:59:12 +0100

    feat: add code to create a carousel

---
+++ constants.js
@@ -0,0 +1,22 @@
+export const IMAGES = [
+  {
+    src: '/images/background-1.jpg',
+    alt: 'background-1',
+  },
+  {
+    src: '/images/background-2.jpg',
+    alt: 'background-2',
+  },
+  {
+    src: '/images/cat.jpg',
+    alt: 'cat',
+  },
+  {
+    src: '/images/dog.jpg',
+    alt: 'dog',
+  },
+  {
+    src: '/images/fox.jpg',
+    alt: 'fox',
+  },
+];
